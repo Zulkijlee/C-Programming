8c9a333a649ac7c1d1df5176e68e93ce80eaa980
MD. Zul-kijee Rahman Tapader <zulki3534@gmail.com>
2026-03-11 02:06:02 +0600

    Add files via upload

---
+++ Variables.c
@@ -0,0 +1,36 @@
+/*#include<stdio.h>
+int main()
+{
+    Using variables with a proper format specifier.
+
+    int age = 20;
+
+    float height = 5.6;
+
+    char grade = 'A';
+
+    printf("Age: %d years", age);
+    printf("\nHeight: %0.1f ", height);
+    printf("\nGrade: %c ", grade);
+
+    return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main()
+{
+    //Changing variable values.
+
+    int num = 15;
+    printf("1st value: %d", num);
+
+    num = 10;
+    printf("\nUpdated value: %d\n", num);
+
+    num = num + 3;
+    printf("Adding: %d\n", num);
+
+    return 0;
+}
+*/
